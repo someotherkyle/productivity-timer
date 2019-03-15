01d7a83bb9b8d6481cc7b7339b2a5cd65fc5efe0
kyle <kylemclellan@gmail.com>
2019-03-15 18:58:31 -0400

    needs: testing, #reset and #help

--- timer.rb
+++ timer.rb
@@ -25,29 +25,63 @@ def open_file()
   file
 end
 
-def main()
-  file = open_file()
-  input = take_input("What are you currently working on?")
-  current = nil
-  while input != 'exit'
-    if current == nil
-      current = input
-    else
-      file[current][:ended] = Time.now
-      file[current][:total] = file[current][:ended] - file[current][:started]
-      file[current].keep_if {|key, value| key == :total}
-      current = input
-    end
-    if file.key?(input)
-      file[input][:started] = Time.now
-    else
-      file[input] = {}
-      file[input][:started] = Time.now
-    end
-    input = take_input("Let me know what you work on after #{input}.")
+def end_current_task(file, current)
+  file[current][:ended] = Time.now
+  if file[current].has_key?(:total)
+    file[current][:total] = file[current][:total] + (file[current][:ended] - file[current][:started])
+  else
+    file[current][:total] = file[current][:ended] - file[current][:started]
   end
-  binding.pry
+  file[current].keep_if {|key, value| key == :total}
 end
 
-main
+def begin_new_task(file, current)
+  if file.key?(current)
+    file[current][:started] = Time.now
+  else
+    file[current] = {}
+    file[current][:started] = Time.now
+  end
+end
 
+def review_totals(file)
+  file.each do |key, value|
+    unless key == :established
+      binding.pry
+      total_time = value[:total]
+      hours = total_time.to_int / 3600
+      total_time -= hours * 3600
+      minutes = total_time.to_int / 60
+      total_time -= minutes * 60
+      puts "You've worked on #{key} for #{hours} hours, #{minutes} minutes, and #{total_time} seconds."
+    end
+  end
+end
+
+file = open_file()
+input = take_input("What are you currently working on?")
+current = nil
+while input != 'exit'
+  if current == nil
+    current = input
+  elsif input == current
+    puts "You are already working on #{current}."
+  end
+
+  if input == 'pause'
+    end_current_task(file, current) unless current == 'review' || current == 'pause'
+    puts "If you would like to resume, press enter."
+    temp = gets
+  elsif input == 'review'
+    end_current_task(file, current) unless current == 'review' || current == 'pause'
+    review_totals(file)
+    next
+  else
+    end_current_task(file, current) unless current == 'review' || current == 'pause'
+    current = input
+  end
+  begin_new_task(file, current) unless current == 'review' || current == 'pause'
+  input = take_input("Let me know what you work on after #{current}.")
+end
+end_current_task(file, current) unless current == 'review' || current == 'pause'
+File.write('log.yml', file.to_yaml)
